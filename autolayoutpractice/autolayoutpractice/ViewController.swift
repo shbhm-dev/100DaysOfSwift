2ca5e3686c7d9b201d2ff8e273638100ab1149b9
shbhm-dev <shubham.15561@sscbs.du.ac.in>
2020-10-29 22:00:12 +0530

    autolayout

---
+++ autolayoutpractice/autolayoutpractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayoutpractice
+//
+//  Created by STARKS on 10/29/20.
+//  Copyright © 2020 STARKS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
